111b15cf2aca19a99eea40bd3010240af7a002f3
Thomas Adams <tqsare@gmail.com>
2020-06-10 10:14:55 -0700

    add create_app

--- app/__init__.py
+++ app/__init__.py
@@ -5,23 +5,38 @@ from .models import db, connect_db
 from .shows.show_routes import shows
 from .users.users_routes import user
 from .auth.auth_routes import auth
-from .users.forms import RegisterUserForm, LoginForm
 from datetime import datetime
 from sqlalchemy.exc import IntegrityError
 
-app = Flask(__name__)
-app.config.from_object('config.ProdConfig')
-toolbar = DebugToolbarExtension(app)
+# app = Flask(__name__)
+# app.config.from_object('config.ProdConfig')
+# toolbar = DebugToolbarExtension(app)
 
-connect_db(app)
+# connect_db(app)
+
+# with app.app_context():
+#     db.create_all()
+
+# app.register_blueprint(shows, url_prefix='/shows')
+# app.register_blueprint(user, url_prefix='/user')
+# app.register_blueprint(auth)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.ProdConfig')
+    connect_db(app)
+    with app.app_context():
+        from .shows.show_routes import shows
+        from .users.users_routes import user
+        from .auth.auth_routes import auth
+        db.create_all()
+
+        app.register_blueprint(shows, url_prefix='/shows')
+        app.register_blueprint(user, url_prefix='/user')
+        app.register_blueprint(auth)
 
 
-with app.app_context():
-    db.create_all()
 
-app.register_blueprint(shows, url_prefix='/shows')
-app.register_blueprint(user, url_prefix='/user')
-app.register_blueprint(auth)
 
 
 @app.route('/')
