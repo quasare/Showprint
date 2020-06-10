98c2bb1f18aaec88c826017801ed6e95fa7594b8
Thomas Adams <tqsare@gmail.com>
2020-06-10 09:50:09 -0700

    add heroku pg

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
-app.config.from_object('config.DevConfig')
+app.config.from_object('config.ProdConfig')
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
