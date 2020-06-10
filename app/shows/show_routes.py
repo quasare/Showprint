34c75b78e0451f36f2dc0908f9047096ac34a625
Thomas Adams <tqsare@gmail.com>
2020-06-09 17:18:40 -0700

    add show_utils

--- app/shows/show_routes.py
+++ app/shows/show_routes.py
@@ -6,20 +6,21 @@ from ..models import db, Search, Show, Season, Episode, \
 from ..utils.auth_utils import login_required
 from ..utils.show_api_utils import seasons_search, seasons_episodes, shows_search
 from ..utils.youtube_utils import youtube_search
-from ..utils.show_utils import toggle_ep, toggle_season
+from ..utils.show_utils import toggle_ep, get_episodes,\
+    watch_show, delete_show, unwatch_show, handle_season
 
 
 shows = Blueprint('shows', __name__, template_folder='templates',
                   static_folder='static')
 
-
-# 
+# Route to search TV shows
 @shows.route('/', methods=['POST', 'GET'])
 @login_required
 def show_home():
     form = SearchForm()
-    if form.validate_on_submit():
 
+    # Validate search form
+    if form.validate_on_submit():
         show = form.search.data
         existing_search = Search.query.filter(
             show == Search.search_value).first()
@@ -27,12 +28,11 @@ def show_home():
             new_search = Search(search_value=show)
             db.session.add(new_search)
             db.session.commit()
-
         session['search'] = show
         return redirect(url_for('shows.search_results'))
     return render_template('show_home.html', form=form)
 
-
+# Search results
 @shows.route('/results')
 @login_required
 def search_results():
@@ -41,101 +41,64 @@ def search_results():
     res = shows_search(show)
     return render_template('search_results.html', res=res, form=form)
 
-
+# Get Episodes for shows
 @shows.route('/submit/<id>')
 @login_required
 def submit(id):
     in_db = Episode.query.filter(Episode.show_id == id).first()
     if not in_db:
-        ep_list = []
-        show = Show.query.get_or_404(id)
-        season = seasons_search(show.api_id)
-        season_list = [Season(season_num=s['season_num'],
-                              api_id=s['api_id'], show_id=show.id) for s in season]
-        db.session.add_all(season_list)
-        db.session.commit()
-        eps = [seasons_episodes(s.api_id) for s in season_list]
-        for s_e in eps:
-            for e in s_e:
-                new_ep = Episode(name=e['name'], season_id=e['season_id'], summary=e['summary'],
-                                 number=e['ep_num'], api_id=e['api_id'], show_id=show.id)
-
-                ep_list.append(new_ep)
-        db.session.add_all(ep_list)
-        db.session.commit()
-
+        get_episodes(id)
         return redirect(url_for('shows.show_detail', id=id))
     return redirect(url_for('shows.show_detail', id=id))
 
-
+# Render show detail page
 @shows.route('/detail/<id>')
 @login_required
 def show_detail(id):
     username = session['username']
     show = Show.query.get_or_404(id)
-    user_season = Watched_season.query.filter()
-    user_ep = Watched_episode.query.filter(Watched_episode.user_id == username).all()
-    recap_vids = youtube_search(f'{show.name} preview') 
 
-    return render_template('show_detail.html', show=show,vid=recap_vids )
+    # Request youtube recap vids
+    recap_vids = youtube_search(f'{show.name} preview')
+    return render_template('show_detail.html', show=show, vid=recap_vids)
 
-
+# Route to handle ajax logic to mark Episodes as watched
 @shows.route('/watch_ep', methods=['POST'])
 @login_required
 def watched_ep():
+    # Handle ajax post from Frontend
     watched_ep = request.json['userEp'].split(' ')
     ep_id, username = watched_ep
-    in_db = Watched_episode.query.filter(
-        Watched_episode.ep_id == ep_id, Watched_episode.user_id == username).first()
+    
     toggle_ep(ep_id, username)
-    return jsonify(res='success')
+    return jsonify({"res": 'success'})
 
-
+# Mark shows a currenlty watching for Current User
 @shows.route('/watching/<id>', methods=['POST', 'GET'])
 @login_required
 def cur_watching_show(id):
-    username = session['username']
-    show = Watched_show.query.filter(
-        Watched_show.user_id == username, Watched_show.show_id == id).first()
-    watching = show.watching = True
-    db.session.commit()
+    watch_show(id)
     return redirect(url_for('user.user_dashboard'))
 
-
-@shows.route('/remove/<id>', methods=['POST', 'GET'])
-@login_required
-def remove_show(id):
-    username = session['username']
-    show = Watched_show.query.filter(
-        Watched_show.user_id == username, Watched_show.show_id == id).first()
-    db.session.delete(show)
-    db.session.commit()
-    return redirect(url_for('user.user_dashboard'))
-
-
+# Remove show from currently watching and add back to show of interest
 @shows.route('/remove_watching/<id>')
 @login_required
 def remove_watching(id):
-    username = session['username']
-    show = Watched_show.query.filter(
-        Watched_show.user_id == username, Watched_show.show_id == id).first()
-    watching = show.watching = False
-    db.session.commit()
+    unwatch_show(id)
     return redirect(url_for('user.user_dashboard'))
 
+#  Delete show from users list of shows
+@shows.route('/remove/<id>', methods=['POST', 'GET'])
+@login_required
+def remove_show(id):
+    delete_show(id)
+    return redirect(url_for('user.user_dashboard'))
 
+# Mark season as watched for AJAX request
 @shows.route('/watch_season', methods=['POST'])
 @login_required
 def watch_season():
-    username = session['username']
     watched_s = request.json['season'].split(' ')
-
     season, id, season_id = watched_s
-    show = Show.query.get_or_404(id)
-    s = (int(season) - 1)
-    ep_list = show.seasons[s].episodes
-    Season_bool = show.seasons[s].watched
-    toggle_season(season_id, username)
-    for e in ep_list:
-        toggle_ep(e.id, username)
+    handle_season(season, id, season_id)
     return redirect(url_for('shows.show_detail', id=id))
--- app/utils/show_utils.py
+++ app/utils/show_utils.py
@@ -2,6 +2,82 @@ from flask import Blueprint, render_template, session,  \
     redirect, url_for, flash, jsonify, request
 from ..models import db, Search, Show, Season, Episode, \
     Watched_episode, Watched_show, Watched_season
+from .show_api_utils import seasons_search, seasons_episodes, shows_search
+
+
+# **************************** SHOW LOGIC ****************************
+
+# Mark show as currently watching for user
+def watch_show(id):
+    username = session['username']
+    show = Watched_show.query.filter(
+        Watched_show.user_id == username, Watched_show.show_id == id).first()
+    watching = show.watching = True
+    db.session.commit()
+
+# Remove show from currently watching and add back to show of interest
+def unwatch_show(id):
+    username = session['username']
+    show = Watched_show.query.filter(
+        Watched_show.user_id == username, Watched_show.show_id == id).first()
+    watching = show.watching = False
+    db.session.commit()    
+
+# Mark show as not currently watching for user
+def delete_show(id):
+    username = session['username']
+    show = Watched_show.query.filter(
+        Watched_show.user_id == username, Watched_show.show_id == id).first()
+    db.session.delete(show)
+    db.session.commit()
+
+
+# *************************** SEASON LOGIC ***************************
+
+# Mark season watched in DB
+def toggle_season(s_id, username):
+    in_db = Watched_season.query.filter(
+        Watched_season.season_id == s_id, Watched_season.user_id == username).first()
+    if not in_db:
+        new_watched_ep = Watched_season(
+            user_id=username, season_id=s_id, finished=True)
+        db.session.add(new_watched_ep)
+        db.session.commit()
+    elif in_db:
+        db.session.delete(in_db)
+        db.session.commit()
+    return
+
+# Take data from front-end and toggle season as watched
+def handle_season(season, id, season_id):
+    username = session['username']
+    show = Show.query.get_or_404(id)
+    season_idx = (int(season) - 1)
+    ep_list = show.seasons[season_idx].episodes
+    toggle_season(season_id, username)
+    for e in ep_list:
+        toggle_ep(e.id, username)
+
+
+# ************************ EPISODE LOGIC ************************
+
+# Logic to query episodes for show
+def get_episodes(id):
+    ep_list = []
+    show = Show.query.get_or_404(id)
+    season = seasons_search(show.api_id)
+    season_list = [Season(season_num=s['season_num'],
+                          api_id=s['api_id'], show_id=show.id) for s in season]
+    db.session.add_all(season_list)
+    db.session.commit()
+    eps = [seasons_episodes(s.api_id) for s in season_list]
+    for s_e in eps:
+        for e in s_e:
+            new_ep = Episode(name=e['name'], season_id=e['season_id'], summary=e['summary'],
+                             number=e['ep_num'], api_id=e['api_id'], show_id=show.id)
+            ep_list.append(new_ep)
+    db.session.add_all(ep_list)
+    db.session.commit()
 
 # Function to mark episode watched in DB
 def toggle_ep(ep_id, username):
@@ -15,18 +91,13 @@ def toggle_ep(ep_id, username):
     elif in_db:
         db.session.delete(in_db)
         db.session.commit()
-    return    
-
-# Marke season watched in DB 
-def toggle_season(s_id, username):
-    in_db = Watched_season.query.filter(
-        Watched_season.season_id == s_id, Watched_season.user_id == username).first()
-    if not in_db:
-        new_watched_ep = Watched_season(
-            user_id=username, season_id=s_id, finished=True)
-        db.session.add(new_watched_ep)
-        db.session.commit()
-    elif in_db:
-        db.session.delete(in_db)
-        db.session.commit()
     return
+
+
+
+
+
+
+
+
+
